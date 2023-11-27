d6999dce65dfd1e9020f78463ef7440d7b8158e9
chayanne <chayanne.morango02@gmail.com>
2023-11-27 11:43:50 -0300

    Primeira aula de Python

---
+++ Aula01/entradadados.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+
+nome = input("Digite o seu nome:")
+print("Olá " +nome)
---
+++ Aula01/par_impar.py
@@ -0,0 +1,12 @@
+numero = input("Digite um número: ")
+#O comando input sempre retorna uma valor 
+#em formato texto. Não importa se foi digitado 
+#um número. Ele sempre retorna um texto
+#Sendo assim, foi necessário converter a variável
+#número para um valor inteiro. Utilizamos o 
+#comando int(inteiro)
+
+if int(numero) % 2 == 0:
+    print("O número digitado é Par")
+else:
+    print("O número digitado é Impar")
