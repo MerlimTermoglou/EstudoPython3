88e33ca23de0585cbb88d9204d746bfa4870a15b
chayanne <chayanne.morango02@gmail.com>
2023-11-29 10:50:23 -0300

    Estruturas de repetição for e while

---
+++ Aula02/for3.py
@@ -0,0 +1,10 @@
+import os
+
+cores = ["Rosa","Verde","Preto","Lilás","Azul","Ciano","Branco","Marrom"]
+os.system("clear")
+#for a in cores :
+   # print("Eu gosto de "+a)
+g = 1
+for a in cores:
+    print(str(g)+"ª a cor é "+a)
+    g=g+1
